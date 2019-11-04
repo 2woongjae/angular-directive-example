8686a89eebcf5c119a169ad3c91b3dc738046421
mark <mark@protopie.io>
2019-11-05 02:18:42 +0900

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-directive-example';
+  title = "angular-directive-example";
+  isVisible = false;
+  list = [
+    { title: "Tour of Heroes" },
+    { title: "CLI Documentation" },
+    { title: "Angular blog" }
+  ];
+  style = {
+    color: "green"
+  };
 }
